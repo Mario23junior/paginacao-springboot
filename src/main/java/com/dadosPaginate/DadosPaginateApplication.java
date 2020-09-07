4dbfa4d5ddeaefd3a50c8d0d8d81d785b452e0c3
Mario_junior23 <mariojunior3251@gmail.com>
2020-09-07 00:36:15 -0300

    Iniciando projeto de paginacao de dados

---
+++ src/main/java/com/dadosPaginate/DadosPaginateApplication.java
@@ -0,0 +1,13 @@
+package com.dadosPaginate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosPaginateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosPaginateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dadosPaginate/DadosPaginateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadosPaginate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosPaginateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
